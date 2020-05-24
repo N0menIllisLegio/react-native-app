6d6b0652e4f12dd84bd34e2d9033a244f829cf73
Dmitry <33730148+N0menIllisLegio@users.noreply.github.com>
2020-05-24 03:30:54 +0300

    Move to SDK version 34

--- components/DataController.js
+++ components/DataController.js
@@ -6,23 +6,23 @@ class DataController {
     static Cart = [];
     
     constructor() {
-    	// db.insert(storage[0], function(er, n){});
-    	// db.insert(storage[1], function(er, n){});
-    	// db.insert(storage[2], function(er, n){});
-    	// db.insert(storage[3], function(er, n){});
-    	// db.insert(storage[4], function(er, n){});
-    	// db.insert(storage[5], function(er, n){});
-    	// db.insert(storage[6], function(er, n){});
-      // db.insert(storage[7], function(er, n){});
+    	db.insert(storage[0], function(er, n){});
+    	db.insert(storage[1], function(er, n){});
+    	db.insert(storage[2], function(er, n){});
+    	db.insert(storage[3], function(er, n){});
+    	db.insert(storage[4], function(er, n){});
+    	db.insert(storage[5], function(er, n){});
+    	db.insert(storage[6], function(er, n){});
+        db.insert(storage[7], function(er, n){});
 
-      db.update({ _id: 1 }, { $set: { inStock: 1 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 2 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 3 }, { $set: { inStock: 4 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 4 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 5 }, { $set: { inStock: 2 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 6 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 7 }, { $set: { inStock: 9 } }, { multi: true }, function (err, numReplaced) { });
-      db.update({ _id: 8 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 1 }, { $set: { inStock: 1 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 2 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 3 }, { $set: { inStock: 4 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 4 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 5 }, { $set: { inStock: 2 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 6 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 7 }, { $set: { inStock: 9 } }, { multi: true }, function (err, numReplaced) { });
+      // db.update({ _id: 8 }, { $set: { inStock: 0 } }, { multi: true }, function (err, numReplaced) { });
     }
 
     static getOneProduct(id, amount) {
@@ -120,7 +120,7 @@ const storage = [
 	    price: 450,
 	    inStock: 0,
 	    photoMain: require('../photos/Photo_4.jpeg'),
-	    extraPhoto1: require('../photos/Photo_5.jpeg'),
+	    extraPhoto1: require('../photos/Photo_5.jpg'),
 	    extraPhoto2: null
 	  },
 	  {
@@ -154,62 +154,62 @@ const storage = [
 	    extraPhoto2: require('../photos/Photo_10.jpeg')
 	},
     {
-    _id: 5,
-    displayDiagonal: 6.5,
-    memorySize: 16,
-    batteryCapacity: 2895,
-    oS: 'Android',
-    frontalCamera: 40,
-    title: 'Motorola One',
-    info: 'Very good product! You should use it!',
-    price: 187.99,
-    inStock: 3,
-    photoMain: require('../photos/Photo_8.jpeg'),
-    extraPhoto1: require('../photos/Photo_9.jpeg'),
-    extraPhoto2: require('../photos/Photo_10.jpeg')
-  },
-  {
-    _id: 6,
-    displayDiagonal: 6.1,
-    memorySize: 32,
-    batteryCapacity: 3000,
-    oS: 'Andorid',
-    frontalCamera: 9,
-    title: 'Redmi Go',
-    info: 'Very good product! You should use it!',
-    price: 322,
-    inStock: false,
-    photoMain: require('../photos/Photo_10.jpeg'),
-    extraPhoto1: require('../photos/Photo_11.jpeg'),
-    extraPhoto2: null
-  },
-  {
-    _id: 7,
-    displayDiagonal: 5.8,
-    memorySize: 32,
-    batteryCapacity: 3500,
-    oS: 'ios',
-    frontalCamera: 12,
-    title: 'Apple iPhone 6',
-    info: 'Very good product! You lkdfkdsnjnj jsndjsnd ksdk ksnd kdfskdflk should use it!',
-    price: 600,
-    inStock: 0,
-    photoMain: require('../photos/Photo_12.jpeg'),
-    extraPhoto1: require('../photos/Photo_13.jpeg'),
-    extraPhoto2: null
-  },
-  {
-    _id: 8,
-    displayDiagonal: 6.3,
-    memorySize: 16,
-    batteryCapacity: 4000,
-    oS: 'Android',
-    frontalCamera: 15,
-    title: 'Samsung Galaxy S8',
-    info: 'Very good product! You should use it!',
-    price: 545.99,
-    inStock: 5,
-    photoMain: require('../photos/Photo_14.jpeg'),
-    extraPhoto1: require('../photos/Photo_15.jpeg'),
-    extraPhoto2: require('../photos/Photo_16.jpeg')
-}]
+        _id: 5,
+        displayDiagonal: 6.5,
+        memorySize: 16,
+        batteryCapacity: 2895,
+        oS: 'Android',
+        frontalCamera: 40,
+        title: 'Motorola One',
+        info: 'Very good product! You should use it!',
+        price: 187.99,
+        inStock: 3,
+        photoMain: require('../photos/Photo_8.jpeg'),
+        extraPhoto1: require('../photos/Photo_9.jpeg'),
+        extraPhoto2: require('../photos/Photo_10.jpeg')
+      },
+      {
+        _id: 6,
+        displayDiagonal: 6.1,
+        memorySize: 32,
+        batteryCapacity: 3000,
+        oS: 'Andorid',
+        frontalCamera: 9,
+        title: 'Redmi Go',
+        info: 'Very good product! You should use it!',
+        price: 322,
+        inStock: false,
+        photoMain: require('../photos/Photo_10.jpeg'),
+        extraPhoto1: require('../photos/Photo_11.jpeg'),
+        extraPhoto2: null
+      },
+      {
+        _id: 7,
+        displayDiagonal: 5.8,
+        memorySize: 32,
+        batteryCapacity: 3500,
+        oS: 'ios',
+        frontalCamera: 12,
+        title: 'Apple iPhone 6',
+        info: 'Very good product! You lkdfkdsnjnj jsndjsnd ksdk ksnd kdfskdflk should use it!',
+        price: 600,
+        inStock: 0,
+        photoMain: require('../photos/Photo_12.jpeg'),
+        extraPhoto1: require('../photos/Photo_13.jpeg'),
+        extraPhoto2: null
+      },
+      {
+        _id: 8,
+        displayDiagonal: 6.3,
+        memorySize: 16,
+        batteryCapacity: 4000,
+        oS: 'Android',
+        frontalCamera: 15,
+        title: 'Samsung Galaxy S8',
+        info: 'Very good product! You should use it!',
+        price: 545.99,
+        inStock: 5,
+        photoMain: require('../photos/Photo_14.jpeg'),
+        extraPhoto1: require('../photos/Photo_15.jpeg'),
+        extraPhoto2: require('../photos/Photo_16.jpeg')
+    }]
